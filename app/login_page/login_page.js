888fd60497d8fbf61c26029bd5bd8dfe2052fc47
alima-kailamarie <12malvar.alimakailamarie@gmail.com>
2024-01-07 03:13:01 +0800

    add login page

---
+++ app/login_page/login_page.js
@@ -0,0 +1,39 @@
+function toggleClearButton() {
+    var inputElement = document.getElementById('user');
+    var clearButton = document.querySelector('.clear-button');
+
+    if (inputElement.value.trim() !== '') {
+        clearButton.style.display = 'block';
+    } else {
+        clearButton.style.display = 'none';
+    }
+}
+
+function clearInput() {
+    document.getElementById('user').value = '';
+    toggleClearButton();
+}
+
+function togglePasswordButton() {
+    var passwordInput = document.getElementById('pass');
+    var toggleButton = document.querySelector('.toggle-password');
+
+    if (passwordInput.value.trim() !== '') {
+        toggleButton.style.display = 'inline-block';
+    } else {
+        toggleButton.style.display = 'none';
+    }
+}
+
+function togglePasswordVisibility() {
+    var passwordInput = document.getElementById('pass');
+    var toggleButton = document.getElementById('togglePassword');
+
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+        toggleButton.innerHTML = '<i class="fas fa-eye-slash"></i>';
+    } else {
+        passwordInput.type = 'password';
+        toggleButton.innerHTML = '<i class="fas fa-eye"></i>';
+    }
+}
